7ee9f14c6fe78125b26a9a293253c8f78aa1a82b
tatianaherrera24 <160152001+HerreraA27@users.noreply.github.com>
2024-08-15 08:25:48 -0600

    "new"

--- controllers/utils/public.js
+++ controllers/utils/public.js
@@ -32,7 +32,7 @@ const loadTemplate = async () => {
                 <header>
                     <nav class="navbar fixed-top navbar-expand-lg bg-body-tertiary">
                         <div class="container">
-                            <a class="navbar-brand" href="index.html"><img src="../../resources/img/LogoDL.png" height="50" alt="Drinking Lover"></a>
+                            <a class="navbar-brand" href="index.html"><img src="../../resources/img/LogoDL.jpeg" height="50" alt="Drinking Lover"></a>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
